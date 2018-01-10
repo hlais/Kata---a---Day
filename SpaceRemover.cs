a26edfb2dad6a360d3ac5564257df40c0b11154d
Halim Lais <halimlais@hotmail.com>
2018-01-10 11:28:30 +0000

    Day 001
    Simple, remove the spaces from the string, then return the resultant string.

---
+++ SpaceRemover.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    public static class SpaceRemover
+    {
+
+        public static string NoSpace(string input)
+        {
+            String result = "";
+
+            result = input.Replace(" ", "");
+
+            return result;
+        }
+    }
+}
