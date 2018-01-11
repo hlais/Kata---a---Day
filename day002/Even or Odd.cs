4c8f31ea56cc7b833146d6b398f9333ec9daa483
Halim Lais <halimlais@hotmail.com>
2018-01-11 17:54:48 +0000

    Add files via upload

---
+++ day002/Even or Odd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    public static class Even_or_Odd
+    {
+        public static string EvenOrOdd(int number)
+        {
+            string EvenOrOdd = "";
+
+            if (number % 2 == 0)
+            {
+                EvenOrOdd = "Even";
+                return EvenOrOdd;
+            }
+            else
+            {
+                EvenOrOdd = "Odd";
+                return EvenOrOdd;
+            }
+            
+        }
+    }
+}
