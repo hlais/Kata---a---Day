608495657e2417eb8a029be93aa76815b47d3dcd
Halim Lais <halimlais@hotmail.com>
2018-01-12 10:47:26 +0000

    Add files via upload

---
+++ day003/Sort_Array_By_String_Length.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace CodeWars
+{
+    public class Sort_Array_By_String_Length
+    {
+        public static string[] SortArrayByStringLength(string[] array)
+        {
+
+
+            string[] sortedArray = new string[array.Length];
+
+
+            Array.Sort(array, (x, y) => x.Length.CompareTo(y.Length));
+            for (int i = 0; i < array.Length; i++)
+            {
+                sortedArray[i] = array[i];
+
+            }
+
+
+
+            return sortedArray;
+
+        }
+    }
+}
+
+
+
+
+
