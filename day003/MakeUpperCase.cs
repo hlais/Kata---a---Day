16c02c2f1d5b0f3a563434a077cd374c58458ecf
Halim Lais <halimlais@hotmail.com>
2018-01-12 11:02:17 +0000

    Add files via upload

---
+++ day003/MakeUpperCase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class Make_Upper_Case
+    {
+        public static string MakeUpperCase(string str)
+        {
+
+            string toUpper = str.ToUpper();
+            return toUpper;
+        }
+
+    }
+}
+
