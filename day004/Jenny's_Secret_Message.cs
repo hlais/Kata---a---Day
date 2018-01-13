4c9588200efd7496149e36139c07cb30cfaff3e2
Halim Lais <halimlais@hotmail.com>
2018-01-13 14:16:38 +0000

    Add files via upload

---
+++ day004/Jenny's_Secret_Message.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class Jenny_s_Secret_Message
+    {
+        public static string greet(string name)
+        {
+            string greeting = "";
+            {
+
+                greeting = "Hello, " + name + "!";
+
+                if (name == "Johnny")
+                {
+                    greeting = "Hello, my love!";
+                }
+                return greeting;
+
+            }
+        }
+    }
+}
+
+   
