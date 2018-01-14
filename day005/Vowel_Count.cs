15e25d2525d9787b7b69ab02e7d715e800d668db
Halim Lais <halimlais@hotmail.com>
2018-01-14 13:32:39 +0000

    Add files via upload

---
+++ day005/Vowel_Count.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CodeWars
+{
+    class Vowel_Count
+    {
+        public static int GetVowelCount(string str)
+        {
+            nt vowelCount = 0;
+            char[] charInString = str.ToCharArray();
+            foreach (var item in charInString)
+            {
+                if (item == 'a' || item == 'e' || item == 'i' || item == 'o' || item == 'u')
+                {
+                    vowelCount++;
+                }
+
+
+            }
+            return vowelCount;
+
+        }
+
+    }
+}
