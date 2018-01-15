685b840ade65155c999260bc3505c6aa6fd3a757
Halim Lais <halimlais@hotmail.com>
2018-01-15 11:12:47 +0000

    Add files via upload

---
+++ Shortest_Word_In_Sentence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CodeWars
+{
+    class Shortest_Word_In_Sentence
+    {
+        public static int FindShort(string sentence)
+        {
+
+            string[] words = sentence.Split();
+            int shortestWordLength = words.Min(p => p.Length);
+            return shortestWordLength;
+
+        }
+    }
+}
+            
+    
