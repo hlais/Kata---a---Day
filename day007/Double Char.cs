bc48c9c98555b8fa71e128b82af0231e9de0a43d
Halim Lais <halimlais@hotmail.com>
2018-01-16 09:00:36 +0000

    Add files via upload

---
+++ day007/Double Char.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class Double_Char
+    {
+        public static string CharDouble(string sentence)
+        {
+            string doubleCharSentence = "";
+            char[] letters = sentence.ToCharArray();
+
+            foreach (var item in letters)
+            {
+                doubleCharSentence += item.ToString() + item.ToString();
+
+            }
+
+            return doubleCharSentence;
+
+        }
+    }
+}
+       
