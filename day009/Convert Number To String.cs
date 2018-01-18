b6cd8b1e8f82cbb33d5515092bb7fb61495116f6
Halim Lais <halimlais@hotmail.com>
2018-01-18 11:23:53 +0000

    Add files via upload

---
+++ day009/Convert Number To String.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class Convert_Number_To_String
+    {
+        public static string NumberToString(int num)
+        {
+           string  NumberToString =  num.ToString();
+
+            return NumberToString;
+        }
+    }
+}
