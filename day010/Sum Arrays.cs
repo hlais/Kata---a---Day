c8d38454f65f702c0a3daa62d26827de7272c6b4
Halim Lais <halimlais@hotmail.com>
2018-01-19 10:55:25 +0000

    Add files via upload

---
+++ day010/Sum Arrays.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class Sum_Arrays
+    {
+        public static double SumArray(double[] array)
+        {
+            
+             double sumOfArrayElements = 0;
+
+             for (int i = 0; i < array.Length; i++)
+             {
+                sumOfArrayElements+= array[i];
+
+             }
+             return sumOfArrayElements;
+  
+        }
+    }
+}
