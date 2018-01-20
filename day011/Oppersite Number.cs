24600c7821845ff5b68391982e9eee70988dfccd
Halim Lais <halimlais@hotmail.com>
2018-01-20 09:48:52 +0000

    Add files via upload

---
+++ day011/Oppersite Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class Oppersite_Number
+    {
+        public static int Opposite(int number)
+        {
+
+            return number * -1;
+        }
+    }
+}
