43ca1081a8eee52b67a4e0cdb888c158daba361a
Halim Lais <halimlais@hotmail.com>
2018-01-21 09:36:28 +0000

    Add files via upload

---
+++ day012/Reversed Sequence.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace CodeWars
+{
+    class Reversed_Sequence
+    {
+        public static int[] ReverseSeq(int n)
+        {
+            List<int> reverseValues = new List<int>();
+
+            while (n > 0)
+            {
+                reverseValues.Add(n--);
+            }
+            return reverseValues.ToArray();
+        }
+    }
+}
