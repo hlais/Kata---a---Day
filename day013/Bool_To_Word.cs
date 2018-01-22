1c496d19a260f873498d9e1e6381fcf8e66c9b33
Halim Lais <halimlais@hotmail.com>
2018-01-22 14:34:55 +0000

    Add files via upload

---
+++ day013/Bool_To_Word.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class Bool_To_Word
+    {
+ 
+        public static string BoolToWord(bool word)
+        {
+           return word==true?"Yes": "No";
+        }
+    }
+}
+
