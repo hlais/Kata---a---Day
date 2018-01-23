7b61c3f3529c02f9b04c29ab70cdeab4534ab309
Halim Lais <halimlais@hotmail.com>
2018-01-23 08:23:42 +0000

    Add files via upload

---
+++ day014/Elements in Two Arry Sum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CodeWars
+{
+    class Elements_in_Two_Arry_Sum
+    {
+        public static int ArrayPlusArray(int[] arr1, int[] arr2)
+        {
+            return arr1.Sum() + arr2.Sum();
+        }
+    }
+}
