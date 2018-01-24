cca09e125942bc4078f39c7ca02516d60354a989
Halim Lais <halimlais@hotmail.com>
2018-01-24 12:29:15 +0000

    Add files via upload

---
+++ day015/Remove First and Last Character In String.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CodeWars
+{
+    class Remove_First_and_Last_Character_In_String
+    {
+        public static string Remove_char(string s)
+        {
+           return s = s.Substring(1, s.Length - 2);
+
+          
+        }
+    }
+}
