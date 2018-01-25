a490143f13679ad77bb5b745b963df692a31e4bd
Halim Lais <halimlais@hotmail.com>
2018-01-25 09:41:52 +0000

    Add files via upload

---
+++ day016/Count Positive Ints And Sum Up Negative Ints.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CodeWars
+{
+    class Count_Positive_Ints_And_Sum_Up_Negative_Ints
+    {
+        public static int[] CountPositivesSumNegatives(int[] input)
+        {
+
+            if (input == null || input.Length == 0)
+            {
+                return new int[] { };
+            }
+            int countPositiveNumber = input.Count(n => n > 0);
+            int addUpAllNegativesNumbers = input.Where(n => n < 0).Sum();
+
+            return new int[] { countPositiveNumber, addUpAllNegativesNumbers };
+        }
+
+    }
+}
