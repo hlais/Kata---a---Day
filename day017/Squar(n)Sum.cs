121fec9174149efeffee7c964bbf98323623db45
Halim Lais <halimlais@hotmail.com>
2018-01-26 08:39:43 +0000

    Add files via upload

---
+++ day017/Squar(n)Sum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CodeWars
+{
+    class Squar_n_Sum
+    {
+        public static int SquareSum(int[] n)
+        {
+
+
+           return  n.Sum(t => t * t);
+
+        }
+    }
+}
+
