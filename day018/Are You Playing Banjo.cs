e8717847c857207dc8d7a6d545215b473c13c00a
Halim Lais <halimlais@hotmail.com>
2018-01-27 10:56:08 +0000

    Add files via upload

---
+++ day018/Are You Playing Banjo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class Are_You_Playing_Banjo
+    {
+        public static string AreYouPlayingBanjo(string name)
+        {
+            return name.ToUpper().Substring(0,1) == "R" ? name + " plays banjo" : name + " does not play banjo";
+       
+        }
+    }
+}
