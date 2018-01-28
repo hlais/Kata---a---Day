1a9af410d841946927a78bf593e2e1547c3f5a85
Halim Lais <halimlais@hotmail.com>
2018-01-28 09:03:50 +0000

    Add files via upload

---
+++ day019/Filling An Array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace CodeWars
+{
+    class Filling_An_Array
+    {
+        public static int[] Arr(int n)
+        {
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] += i;
+            }
+
+            return arr;
+        }
+    }
+}
+        
