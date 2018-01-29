5785349a630703ff02bc8d7fd742997e498f3a2b
Halim Lais <halimlais@hotmail.com>
2018-01-29 21:00:54 +0000

    Add files via upload

---
+++ day020/Occurances In Array.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace CodeWars
+{
+    class Occurances_In_Array
+    {
+        public static int CountSheeps(bool[] sheeps)
+        {
+            return sheeps.Count(s => s);
+        }
+    }
+}
