998229e4837a2d35634b7d04235eb7409ceab4f1
Halim Lais <halimlais@hotmail.com>
2018-01-30 14:40:11 +0000

    Add files via upload

---
+++ day021/Finding String In Array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace CodeWars
+{
+    class Finding_String_In_Array
+    {
+        public static string FindNeedle(object[] haystack)
+        {
+            int indexOfString = Array.IndexOf(haystack, "needle");
+
+            return "found the needle at position " + indexOfString;
+
+
+
+        }
+    }
+}
+
