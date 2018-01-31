2737419890cab16b064a0b0f7db656f209ff30d1
Halim Lais <halimlais@hotmail.com>
2018-01-31 10:43:05 +0000

    Add files via upload

---
+++ day022/Sum of Array Without Highest and Lowest Number.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CodeWars
+{
+    class Sum_of_Array_Without_Highest_and_Lowest_Number
+    {
+        public static int Sum(int[] numbers)
+        {
+
+           if (numbers == null || numbers.Length == 0) return 0;
+
+           int[] organNumber =   numbers.OrderByDescending(n=> n).ToArray();
+           int sum = 0;
+           for (int i = 1; i < numbers.Length - 1; i++)
+           {
+               sum = sum + organNumber[i]; 
+           }
+
+           return sum;
+           
+
+        }
+    }
+}
