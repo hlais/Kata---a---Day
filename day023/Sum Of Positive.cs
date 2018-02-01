0fa342811acc67fbe63846fe1fb084ab949cb7bc
Halim Lais <halimlais@hotmail.com>
2018-02-01 11:57:04 +0000

    Add files via upload

---
+++ day023/Sum Of Positive.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CodeWars
+{
+    class Sum_Of_Positive
+    {
+        public static int PositiveSum(int[] arr)
+        {
+            if (arr == null || arr.Length == 0)
+                return 0;
+            int sum = 0;
+            foreach (var item in arr)
+            {
+                if (item > 0)
+                {
+                    sum += item;
+
+                }
+            }
+
+            return sum;
+        }
+    }
+}
