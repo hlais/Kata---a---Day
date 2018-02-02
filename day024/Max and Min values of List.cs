0018a9942ce8fb2fec495e9cbfb3e77c568b8798
Halim Lais <halimlais@hotmail.com>
2018-02-02 18:23:33 +0000

    Add files via upload

---
+++ day024/Max and Min values of List.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace CodeWars
+{
+    class Max_and_Min_values_of_List
+    {
+        public int Min(int[] list)
+        {
+            return list.Min();
+        }
+
+        public int Max(int[] list)
+        {
+            return list.Max();
+        }
+    }
+}
