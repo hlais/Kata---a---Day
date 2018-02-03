f2b3ffbd7383bfa56b2b4c235a9cc5e49337219f
Halim Lais <halimlais@hotmail.com>
2018-02-03 13:47:55 +0000

    Add files via upload

---
+++ day025/Get the Middle Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class Get_the_Middle_Character
+    {
+        public static string GetMiddle(string s)
+        {
+            int lenghOfString = s.Length;
+
+            string middle = "";
+            if (lenghOfString % 2 == 0)
+            {
+
+              int newMiddle = (lenghOfString / 2) - 1;
+                middle = s.Substring(newMiddle,2);
+            }
+            if (lenghOfString % 2 != 0)
+            {
+               middle = s.Substring(lenghOfString / 2,1);
+            }
+            return middle;
+  
+        }
+    }
+}
