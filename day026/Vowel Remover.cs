966dd3b751fd0cf42430648259f8e93ee9f0ce88
Halim Lais <halimlais@hotmail.com>
2018-02-04 09:31:18 +0000

    Add files via upload

---
+++ day026/Vowel Remover.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CodeWards7Kata
+{
+    class Vowel_Remover
+    {
+        public static string Disemvowel(string str)
+        {
+            
+            var vowels = new string[] { "a", "e", "i", "o", "u",
+                                           "A", "E", "I", "O", "U"};
+            foreach (var letter in vowels)
+            {
+                str = str.Replace(letter, string.Empty);
+            }
+
+            return str;
+        }
+    }
+}
