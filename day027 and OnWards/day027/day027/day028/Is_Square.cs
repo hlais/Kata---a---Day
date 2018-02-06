496ce0acf5157927e0e1da67e16d6dea15616979
Halim Lais <halimlais@hotmail.com>
2018-02-06 07:54:24 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day028/Is_Square.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class Is_Square
+    {
+        public static bool IsSquare(int n)
+        {
+           if ((Math.Sqrt(n) % 1) == 0)
+   
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
