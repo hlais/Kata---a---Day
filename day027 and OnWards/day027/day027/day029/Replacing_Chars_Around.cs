69bc43ebfdd82aae409f8b6030bce2a02114b288
Halim Lais <halimlais@hotmail.com>
2018-02-07 10:02:34 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day029/Replacing_Chars_Around.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class Replacing_Chars_Around
+    {
+        public static string MakeComplement(string dna)
+        {
+            char[] lettersInDna = dna.ToCharArray();
+            string empty = "";
+            for (int i = 0; i < dna.Length; i++)
+            {
+                if (lettersInDna[i] == 'A')
+                {
+                    empty += 'T';
+                }
+                if (lettersInDna[i] == 'T')
+                {
+                    empty += 'A';
+                }
+                if (lettersInDna[i] == 'G')
+                {
+                    empty +=  'C';
+                }
+                if (lettersInDna[i] == 'C')
+                {
+                    empty +='G';
+                }
+            }
+            return empty;
+   
+        }
+    }
+}
+
+           
