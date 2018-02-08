61e654776530622434741345bd8bb300a3731d7f
Halim Lais <halimlais@hotmail.com>
2018-02-08 11:14:59 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day030/Highest and Lowest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CodeWards7Kata
+{
+    class Highest_and_Lowest
+    {
+        public static string HighAndLow(string numbers)
+        {
+
+            string[] numbersSplit = numbers.Split();
+
+            int converted = numbersSplit.Select(int32);
+            int largest = converted.Max();
+            int smallest = converted.Min();
+
+            return largest + " " + smallest;
+
+
+
+        }
+    }
+}
