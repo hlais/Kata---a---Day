52d5834e05cc3aa18aac6ad9af4b936c7abb78af
Halim Lais <halimlais@hotmail.com>
2018-02-09 09:49:17 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day031/Square Every Digit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CodeWards7Kata
+{
+    class Square_Every_Digit
+    {
+        public static int SquareDigits(int n)
+        {
+            string numbers = n.ToString();
+            char[] charDigits = numbers.ToCharArray();
+
+            string empty = "";
+
+            foreach (var item in charDigits)
+            {
+                int temp =  (int)Char.GetNumericValue(item);
+                int square = temp * temp;
+
+                empty += square.ToString();
+            }
+
+           return Convert.ToInt32(empty);
+
+        }
+    }
+}
