392c46dcf4bf2a41c4ff1548c6db6d8717cda774
Halim Lais <halimlais@hotmail.com>
2018-02-10 10:25:28 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day032/Descending Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CodeWards7Kata
+{
+    class Descending_Order
+    {
+        public static int DescendingOrder(int num)
+        {
+            char[] arr = num.ToString().ToCharArray();
+            Array.Sort(arr);
+            Array.Reverse(arr);
+            return Convert.ToInt32(new string(arr));
+
+
+        }
+    }
+}
+
