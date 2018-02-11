86c6080d815825707de39e8f6c56ff93a005def7
Halim Lais <halimlais@hotmail.com>
2018-02-11 15:17:15 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day033/Char Count to Bool.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+using System.Linq;
+
+namespace CodeWards7Kata
+{
+    class Char_Count_to_Bool
+    {
+        public static bool XO(string input)
+        {
+            List<char> arr = new List<char>(input.ToString().ToLower().ToCharArray());
+
+            int x = arr.Count(f => f == 'x');
+            int y = arr.Count(f => f == 'o');
+
+            if (x == y)
+            {
+                return true;
+            }
+            else return
+                false;
+          
+            
+
+
+
+
+        }
+    }
+}
