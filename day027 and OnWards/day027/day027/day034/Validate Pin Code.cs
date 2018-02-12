0657562c3dd56289f0a182aeed02021d969edf55
Halim Lais <halimlais@hotmail.com>
2018-02-12 10:26:15 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day034/Validate Pin Code.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CodeWards7Kata
+{
+    class Validate_Pin_Code
+    {
+        public static bool ValidateCode(string pin)
+        {
+            foreach (char c in pin)
+            {
+                if (!char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+           
+
+            if (pin.Length == 4 || pin.Length == 6)
+                return true;
+            return false;
+
+        }
+    }
+}
