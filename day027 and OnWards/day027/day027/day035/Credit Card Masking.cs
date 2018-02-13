57ee7c99005e87dac344c79e820fc9c0ac4b9560
Halim Lais <halimlais@hotmail.com>
2018-02-13 08:14:09 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day035/Credit Card Masking.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class Credit_Card_Masking
+    {
+        public static string Maskify(string cc)
+        {
+            char[] letters = cc.ToCharArray();
+            string empty = "";
+            if (letters.Length <= 4)
+            {
+                return cc;
+            }
+
+            for (int i = 0; i < cc.Length -4; i++)
+            {
+                empty += "#";
+            }
+           return empty + cc.Substring(cc.Length - 4);
+            
+        }
+    }
+}
