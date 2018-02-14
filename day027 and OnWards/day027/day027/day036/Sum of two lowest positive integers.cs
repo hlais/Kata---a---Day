fc86e69abf99c0aa690f3b59899c1816d78e07f1
Halim Lais <halimlais@hotmail.com>
2018-02-14 11:21:49 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day036/Sum of two lowest positive integers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CodeWards7Kata
+{
+    class Sum_of_two_lowest_positive_integers
+    {
+        public static int sumTwoSmallestNumbers(int[] numbers)
+        {
+            int totalOfTwoLowestInts;
+            Array.Sort(numbers);
+
+            return totalOfTwoLowestInts = numbers[0] + numbers[1];
+                
+        }
+    }
+}
