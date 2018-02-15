e165155002ced751056bc5e8b92a5874a9ff7a74
Halim Lais <halimlais@hotmail.com>
2018-02-15 14:04:13 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day037/Isograms.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CodeWards7Kata
+{
+    class Isograms
+    {
+        
+        public static bool IsIsogram(string str)
+        {
+            int repeatedCharCount = 0;
+            List<char> listChar = new List<char>(str.ToString().ToLower().ToCharArray());
+            char[] strChar = str.ToLower().ToCharArray();
+            for (int i = 0; i < strChar.Length; i++)
+            {
+                x = listChar.Count(f => f == strChar[i]);
+                {
+                    if (repeatedCharCount == 2)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+                return true;
+
+        }
+    }
+}
