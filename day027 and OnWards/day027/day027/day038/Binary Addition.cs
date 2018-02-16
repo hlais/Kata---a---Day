5535c89995119e5efac1740c13fead1db34eb692
Halim Lais <halimlais@hotmail.com>
2018-02-16 14:44:59 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day038/Binary Addition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class Binary_Addition
+    {
+        public static string AddBinary(int a, int b)
+        {
+            int sum = a + b;
+            string binary = Convert.ToString(sum, 2);
+
+            return binary;
+        }
+    }
+}
