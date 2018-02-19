4e103bedb69ca23996489070d52c4ff03add63e7
Halim Lais <halimlais@hotmail.com>
2018-02-19 10:42:52 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day041/SpinWords.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class SpinWords
+    {
+        public static string Spin_Words (string sentence)
+        {
+            string empty = "";
+            string[] words = sentence.Split();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length >= 5)
+                {
+                    string output = new string(words[i].ToCharArray().Reverse().ToArray());
+                    empty += output + " ";
+                }
+
+                if (words[i].Length < 5)
+                {
+                    empty += words[i] + " ";
+                }
+            }
+            
+            return empty.TrimEnd();
+
+        }
+    }
+}
