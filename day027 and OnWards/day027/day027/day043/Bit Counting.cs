0ffdcfc1d75c90bd4edd3f6334b1aa0b28098950
Halim Lais <halimlais@hotmail.com>
2018-02-21 10:35:40 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day043/Bit Counting.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class Bit_Counting
+    {
+        public static int CountBits(int n)
+        {
+            int count = 0;
+            string binary = Convert.ToString(n, 2);
+
+            char[] binChar = binary.ToCharArray();
+
+            foreach (var item in binChar)
+            {
+                if (item == '1')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+        
+}
