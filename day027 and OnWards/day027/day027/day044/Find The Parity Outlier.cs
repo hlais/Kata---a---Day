ee71b9f9420cb8ad11203cf71a6e701175b73f1b
Halim Lais <halimlais@hotmail.com>
2018-02-22 22:02:51 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day044/Find The Parity Outlier.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class Find_The_Parity_Outlier
+    {
+        public static int Find(int[] integers)
+        {
+            List<int> even = new List<int>();
+            List<int> odd = new List<int>();
+
+            for (int i = 0; i < integers.Length; i++)
+            {
+                if (integers[i] % 2 == 0)
+                {
+                    even.Add(integers[i]);
+
+                }
+                else
+                {
+                    odd.Add(integers[i]);
+                }
+            }
+            if (even.Count > odd.Count)
+            {
+                return odd[0];
+            }
+            else return even[0];
+
+        }
+    }
+}
+
