03793780a844c9d2b4704c210544d1726affda7c
Halim Lais <halimlais@hotmail.com>
2018-02-23 11:15:03 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day045/Find Odd Int.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class Find_Odd_Int
+    {
+        public static int find_it(int[] seq)
+        {
+
+            return seq.GroupBy(num => num).Single(group => group.Count() % 2 != 0).First();
+        }
+    }
+}
+
