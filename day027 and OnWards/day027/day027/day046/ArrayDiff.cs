e07eab3c8e459085245b480c26104b9b7e754e6b
Halim Lais <halimlais@hotmail.com>
2018-02-24 10:18:43 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day046/ArrayDiff.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class ArrayDiff
+    {
+        public static int[] Array_Diff(int[] a, int[] b)
+        {
+            List<int> mainArray = new List<int>(a);
+            foreach (var item in b)
+            {
+                if (mainArray.Contains(item))
+                {
+
+                    mainArray.RemoveAll(n => n == item);
+
+                }
+            }
+            return mainArray.ToArray();
+
+        }
+    }
+}
