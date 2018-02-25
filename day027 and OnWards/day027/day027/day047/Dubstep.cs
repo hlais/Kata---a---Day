8eeef61fb985452e84efceb4580f2917e6ec6288
Halim Lais <halimlais@hotmail.com>
2018-02-25 20:07:23 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day047/Dubstep.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars6Kata
+{
+    class Dubstep
+    {
+        public static string SongDecoder(string input)
+        {
+            List<string> newWordsList = new List<string>();
+            string empty = "";
+            string withOutWub = input.Replace("WUB", " ").Trim(); ;
+
+            string[] newWords = withOutWub.Split();
+            foreach (var item in newWords)
+            {
+                if (item == " " || item == "")
+                {
+                    continue;
+                }
+                newWordsList.Add(item);
+
+            }
+
+            foreach (var item in newWordsList)
+            {
+                empty += item + " ";
+            }
+            return empty.Trim();
+
+
+
+
+        }
+    }
+}
