3cc4a32c0e838b859fd64b475e6a5beff6bfaac2
Halim Lais <halimlais@hotmail.com>
2018-02-26 10:06:11 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day048/Create Phone Number.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class Create_Phone_Number
+    {
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            return string.Format("({0}{1}{2}) {3}{4}{5}-{6}{7}{8}{9}", numbers.Select(x => x.ToString()).ToArray());
+        }
+    }
+}
