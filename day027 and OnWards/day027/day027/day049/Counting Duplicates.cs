5a57d3e9124a72b51fa10e4d02d42bdbfc062f0a
Halim Lais <halimlais@hotmail.com>
2018-02-27 14:50:10 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day049/Counting Duplicates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class Counting_Duplicates
+    {
+        public static int DuplicateCount(string str)
+        {
+            int count = 0;
+            string lowerStr = str.ToLower();
+            
+            foreach (var item in lowerStr.ToLower().Where(n => lowerStr.Count(n2 => n2 == n) > 1).Distinct())
+            {
+                
+                count++;
+            }
+
+            return count;
+            
+        }
+    }
+}
