ce890756ed0c0c8bcccfa0386435e71384a7dc1a
Halim Lais <halimlais@hotmail.com>
2018-02-28 21:10:26 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day050/IQTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class IQTest
+    {
+        public static int Test(string numbers)
+        {
+            int[] stringToNumbersArray= Array.ConvertAll(numbers.Split(' '), int.Parse);
+            List<int> intListNumbers = new List<int>(stringToNumbersArray);
+     
+            int thatNumber = intListNumbers.GroupBy(num => num % 2).Single(group => group.Count() == 1).First();
+            return intListNumbers.IndexOf(thatNumber) +1; 
+        }
+    }
+}
