ba1d0740f45689da11db9d0be0a4a72d5a4d0086
Halim Lais <halimlais@hotmail.com>
2018-03-03 20:27:14 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day053/ReplaceAlphapetPostions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class ReplaceAlphapetPostions
+    {
+        public static string AlphabetPosition(string text)
+        {
+            string numberText = "";
+            foreach (var chars in text.Replace(" ", ""))
+            {
+               int value = char.ToUpper(chars) - 64;
+
+                if (value <= 26 && value > 0)
+                {
+                    numberText += value.ToString() + " ";
+                }
+                else 
+                {
+                    continue;
+                }
+
+            }
+            return numberText.Trim();
+        }
+    }
+}
