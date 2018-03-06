906595924b97e8144380c252db358b1b400bf8c7
Halim Lais <halimlais@hotmail.com>
2018-03-06 19:34:22 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day056/Find the missing letter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class Find_the_missing_letter
+    {
+        public static char FindMissingLetter(char[] array)
+        {
+            List<int> charNumber = new List<int>();
+            int missingCharIndex = 0;
+            foreach (var chars in array)
+            {
+             
+              int value =  (int)chars;
+              charNumber.Add(value);
+
+            }
+
+
+
+            for (int i = 0; i < charNumber.Count; i++)
+            {
+                int j = i + 1;
+                try
+                {
+                    if (charNumber[j] - charNumber[i] != 1)
+                    {
+                        missingCharIndex = charNumber[i];
+                        break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    continue;
+                }
+            }
+            missingCharIndex =  missingCharIndex + 1;
+            char key = (char)(missingCharIndex);
+
+            return key;
+
+        }
+
+    }
+}
