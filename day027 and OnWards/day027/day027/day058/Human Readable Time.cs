27d1850ccf1abc0f380c1a1f70fc64f6dd90a35a
Halim Lais <halimlais@hotmail.com>
2018-03-08 15:57:06 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day058/Human Readable Time.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars5Kata
+{
+    class Human_Readable_Time
+    {
+        public static string GetReadableTime(int seconds)
+        {
+            int hours = seconds / 3600;
+            int mins = (seconds % 3600) / 60;
+            seconds = seconds % 60;
+            return string.Format("{0:D2}:{1:D2}:{2:D2}", hours, mins, seconds);
+        }
+    }
+}
