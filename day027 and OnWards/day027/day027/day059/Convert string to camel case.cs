52b622b4b24458588c57b1de47e367927859e1ad
Halim Lais <halimlais@hotmail.com>
2018-03-09 15:51:36 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day059/Convert string to camel case.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodeWars5Kata
+{
+    class Convert_string_to_camel_case
+    {
+        public static string ToCamelCase(string str)
+        {
+            string _toCamelCase = "";
+           
+            if (str.Contains("_") || str.Contains("-"))
+            {
+                string removedPunc = str.Replace("_", " ");
+                string removedPunc2 = removedPunc.Replace("-", " ");
+                string[] wordCollection = removedPunc2.Split(' ');
+                _toCamelCase = CamelCase(wordCollection).Trim();
+            }
+            return _toCamelCase;
+        }
+        public static string CamelCase(string[] wordCollection)
+        {
+            string empty = "";
+            for (int word = 0; word < wordCollection.Length; word++)
+            {
+                if (word == 0)
+                {
+                    empty += wordCollection[word].ToString();
+                    continue;
+                }
+                else
+                {
+                    char[] chars = wordCollection[word].ToCharArray();
+                    for (int letter = 0; letter < wordCollection[word].Length; letter++)
+                    {
+                        if (letter == 0)
+                        {
+                            empty = empty + char.ToUpper(chars[letter]);
+                        }
+                        else
+                        {
+                            empty += char.ToLower(chars[letter]);
+                        }
+                    }
+                }
+            }
+            return empty;
+        }
+    }
+}
