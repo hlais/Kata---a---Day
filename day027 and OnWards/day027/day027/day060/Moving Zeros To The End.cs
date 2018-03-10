e1c6c5afb0bbe9ae4d42f82213b6e0dd5f9ad400
Halim Lais <halimlais@hotmail.com>
2018-03-10 11:10:14 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day060/Moving Zeros To The End.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars5Kata
+{
+    class Moving_Zeros_To_The_End
+    {
+        public static int[] MoveZeroes(int[] arr)
+        {
+            int zeroCount = 0;
+            List<int> arrList = new List<int>();
+            foreach (var item in arr)
+            {
+                if (item == 0)
+                {
+                    zeroCount++;
+                }
+                else
+                {
+                    arrList.Add(item);
+                }
+            }
+            while (zeroCount != 0)
+            {
+                arrList.Add(0);
+                zeroCount--;
+            }
+            return arrList.ToArray();
+        }
+    }
+}
