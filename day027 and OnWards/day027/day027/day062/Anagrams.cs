a6db4f92f4d550921d765b6123a7bf14b48d6f26
Halim Lais <halimlais@hotmail.com>
2018-03-12 08:10:20 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day062/Anagrams.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars5Kata
+{
+    class Anagrams
+    {
+        public static List<string> FindTheAnagrams(string word, List<string> words)
+        {
+            string orderedWord =  string.Concat(word.OrderBy(c => c));
+            List<string> foundAnagrams = new List<string>();
+            foreach (var item in words)
+            {
+               string tempOrderdWord = string.Concat(item.OrderBy(c => c));
+
+                if (orderedWord == tempOrderdWord)
+                {
+                    foundAnagrams.Add(item);
+                }
+            }
+            return foundAnagrams;
+        }
+    }
+}
