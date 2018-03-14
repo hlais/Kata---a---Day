10a6b8d8121bb30378f9d96085c50d2f4f037cab
Halim Lais <halimlais@hotmail.com>
2018-03-14 12:27:34 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day064/Palindrome_Chain_Length.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CodeWars5Kata
+{
+
+    class Palindrome_Chain_Length
+    {
+
+        public static int PalindromeChainLength(long n)
+        {
+            
+            ProuducingPalidrome(n);
+            bool isNotPalidrome = false;
+            int attempt = 0;
+
+            while (!isNotPalidrome)
+            {
+               isNotPalidrome = IsPalidromeOrNot(n);
+                if (isNotPalidrome == true)
+                {
+                    break;
+                }
+                else
+                {                    
+                   attempt++;
+                   n = ProuducingPalidrome(n) ;
+                }
+            }
+            return attempt;
+        }
+
+        private static bool IsPalidromeOrNot(long n)
+        {
+            string original = n.ToString();
+            string reversed = new string (original.ToCharArray().Reverse().ToArray());
+            
+            if (reversed == original)
+            {
+                return true;
+            }
+            else
+                return false;
+
+        }
+
+        private static long ProuducingPalidrome(long n)
+        {
+            string reversed = new string(n.ToString().ToCharArray().Reverse().ToArray());   
+            return n += Convert.ToInt64(reversed);
+        }
+    }
+}
+
