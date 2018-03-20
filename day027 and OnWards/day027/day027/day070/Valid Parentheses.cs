78654b944aa198d267654f60472b747642f99dca
Halim Lais <halimlais@hotmail.com>
2018-03-20 09:29:19 +0000

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day070/Valid Parentheses.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars5Kata
+{
+    class Valid_Parentheses
+    {
+        public static bool ValidParentheses(string input)
+        {
+            int bracketBalance = 0;
+           
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == ')')
+                {
+                    if (bracketBalance < 1)
+                    {
+                        return false;
+                    }
+                    else bracketBalance--;
+                }
+                else if (input[i] == '(')
+                {
+                    bracketBalance++;
+                }
+            }
+            if (bracketBalance == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
