4da2ff77f0b9cf19a1e84f51e05a0f2b230726aa
Halim Lais <halimlais@hotmail.com>
2018-03-27 19:44:56 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day077/Decode Morse Code.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class Decode_Morse_Code
+    {
+        public static string Decode(string morseCode)
+        {
+
+            string empty = "";
+            Dictionary<string, String> morseCodeDictionary = new Dictionary<string, String>()
+            {
+                {"SOS", "...---..." }, { "!", "-.-.--"}, {".", ".-.-.-"} ,{"a" , ".-"},{"b" , "-..."},{"c" , "-.-."}, //alpha
+                {"d" , "-.."},{"e" , "."},{"f" , "..-."},
+                {"g" , "--."},{"h" , "...."},{"i" , ".."},
+                {"j" , ".---"},{"k" , "-.-"},{"l" , ".-.."},
+                {"m" , "--"},{"n" , "-."},{"o" , "---"},
+                {"p" , ".--."},{"q" , "--.-"},{"r" , ".-."},
+                {"s" , "..."},{"t" , "-"},{"u" , "..-"},
+                {"v" , "...-"},{"w" , ".--"},{"x" , "-..-"},
+                {"y" , "-.--"},{"z" , "--.."},
+                //Numbers 
+                {"0" , "-----"},{"1" , ".----"},{"2" , "..----"},{"3" , "...--"},
+                {"4" , "....-"},{"5" , "....."},{"6" , "-...."},{"7" , "--..."},
+                {"8" , "---.."},{"9" , "----."}
+            };
+
+            string[] morseWords = morseCode.Split();
+            int counter = 1;
+            foreach (var item in morseWords)
+            {
+                if (item == "")
+                {
+                    counter++;
+                    if (counter % 2 == 0)
+                    {
+                        empty += " ";
+
+                    }
+
+                }
+
+                if (morseCodeDictionary.ContainsValue(item))
+                {
+                    var myKey = morseCodeDictionary.FirstOrDefault(x => x.Value == item).Key;
+                    empty += myKey;
+                }
+            }
+        
+            return empty.Trim().ToUpper().TrimEnd();
+        }
+    }
+    
+}
