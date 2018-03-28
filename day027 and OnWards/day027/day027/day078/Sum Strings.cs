c2d61ff30b239a4d8e74025148e52bf55d68ae95
Halim Lais <halimlais@hotmail.com>
2018-03-28 14:28:07 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day078/Sum Strings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+
+namespace CodeWars4Kata
+{
+    class Sum_Strings
+    {
+        public static string SumStrings(string a, string b)
+        {
+            BigInteger number1;
+            BigInteger number2;
+
+            BigInteger.TryParse(a, out number1);
+            BigInteger.TryParse(b, out number2);
+            
+            BigInteger sum;
+            sum = number1 + number2;
+
+
+            
+
+            return sum.ToString();
+
+           
+        }
+
+
+    }
+}
