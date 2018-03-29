36c6d8252f05168c0136cf5fb24daff4cc9be70c
Halim Lais <halimlais@hotmail.com>
2018-03-29 22:36:51 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day079/Occuring Characters.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class Occuring_Characters
+    {
+        public static Dictionary <char, int> OccuringCharacters (string str)
+        {
+            Dictionary<char, int> charCounting = new Dictionary<char, int>();
+
+            foreach (var item in str)
+            {   //if the list already contains key we should go to next item
+                if (charCounting.ContainsKey(item))
+                { 
+                    continue;
+                }
+                    int count = str.Count(f => f == item);
+                    charCounting.Add(item, count);
+                
+            }
+            return charCounting;
+      
+        }
+    }
+}
