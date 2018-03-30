5195828abd6fc9a82acb0a702d391c43fafd331e
Halim Lais <halimlais@hotmail.com>
2018-03-30 09:45:23 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day080/Find the unique number.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class Find_the_unique_number
+    {
+        public static int GetUnique(IEnumerable<int> numbers)
+        {
+          int unique =  numbers.GroupBy(group => group).Single(group => group.Count() == 1).First();
+            return unique;
+        }
+    }
+}
