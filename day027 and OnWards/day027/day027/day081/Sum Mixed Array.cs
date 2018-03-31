d1ca7d44bbf9089de2c2dee0a5429a93a3320b64
Halim Lais <halimlais@hotmail.com>
2018-03-31 13:31:55 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day081/Sum Mixed Array.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class Sum_Mixed_Array
+    {
+        public static int SumMix(object[] x)
+        {
+            int sum = 0;
+            
+            foreach (var item in x)
+            {
+
+              sum +=  Convert.ToInt32(item);
+            }
+            return sum;
+        }
+    }
+}
