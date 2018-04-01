48a52468b48ba70a24c4bde29679436ad1c6ba4b
Halim Lais <halimlais@hotmail.com>
2018-04-01 14:35:32 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day082/Linked List to String.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class Linked_List_to_String
+    {
+        public static string Stringify(Node list)
+        {
+           
+               return list == null ? "null" : $"{list.Data} -> { Stringify(list.Next)}";
+            
+        }
+    }
+    public class Node
+    {
+        public int Data { get; private set; }
+        public Node Next { get; private set; }
+
+        public Node(int data, Node next = null)
+        {
+            Data = data;
+            Next = next;
+        }
+    }
+
+}
+
