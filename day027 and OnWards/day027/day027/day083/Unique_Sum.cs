724645432e0d58f1ef284647d42ff5e3392c75af
Halim Lais <halimlais@hotmail.com>
2018-04-02 07:06:55 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day083/Unique_Sum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+    
+
+namespace CodeWards7Kata
+{
+    class Unique_Sum
+    {
+        public static int UniqueSum(List<int> lst)
+        {
+
+
+            if (lst.Count == 0)
+            {
+                return  null;
+            }
+            return lst.Distinct().Sum();
+
+        }
+    }
+}
