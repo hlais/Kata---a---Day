6da18d04d5aa009e1c1e0c19d3940bdba4300e99
Halim Lais <halimlais@hotmail.com>
2018-04-10 18:30:58 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day085/NthFibonacci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+   public static class NthFibonacci
+    {
+        public static int NthFib(int n)
+        {
+            
+            int a = 0;
+            int b = 1;
+            // In N steps compute Fibonacci sequence iteratively.
+            for (int i = 0; i < n-1; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+    }
+}
