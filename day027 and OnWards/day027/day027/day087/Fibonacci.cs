6eb2e38c1a3710ce32704c10feff6cd3301b81f8
Halim Lais <halimlais@hotmail.com>
2018-04-12 12:39:14 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day087/Fibonacci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class Fibonacci
+    {
+        public static int fib(int n)
+        {
+            int a = 0;
+            int b = 1;
+            // In N steps compute Fibonacci sequence iteratively.
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+    }
+}
