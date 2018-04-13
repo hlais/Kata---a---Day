2d11457ac74ebe3be0327f7aa9ecf35c4c641a3a
Halim Lais <halimlais@hotmail.com>
2018-04-13 21:34:18 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day088/FibonacciAndFriends.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class FibonacciAndFriends
+    {
+        public static double[] xbonacci(double[] signature, int n)
+        {
+
+            double[] fibonacciElements = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i < signature.Length)
+                {
+                    fibonacciElements[i] = signature[i];
+                }
+                else
+                {
+                    for (int j  = 1 ; j <= signature.Length; j ++)
+                    {
+                        fibonacciElements[i] += fibonacciElements[i - j];
+                    }
+                }
+
+            }
+            return fibonacciElements;
+        }
+    }
+}
