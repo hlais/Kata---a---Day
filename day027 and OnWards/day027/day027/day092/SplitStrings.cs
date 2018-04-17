3c9ef9a3a45537c9ca92e0f40ae03402f7e4daa1
Halim Lais <halimlais@hotmail.com>
2018-04-17 21:05:12 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day092/SplitStrings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class SplitStrings
+    {
+        public static string[] Solution(string str)
+        {
+            int splitter = 0;
+            
+            string[] splitStrings = new string[(int)Math.Ceiling((decimal)str.Length / 2)];
+            if (str.Length % 2 == 0)
+            {
+
+                for (int i = 0; i < splitStrings.Length; i++)
+                {
+                    splitStrings[i] = str.Substring(splitter, 2);
+                    splitter += 2;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < splitStrings.Length; i++)
+                {
+                    if (i != splitStrings.Length-1 )
+                    {
+                        splitStrings[i] = str.Substring(splitter, 2);
+                        splitter += 2;
+                        continue;
+                    }
+                    
+                        splitStrings[i] = str.Substring(str.Length - 1) +"_";
+                }
+            }
+
+            return splitStrings;
+        }
+    }
+}
