edc018a345085fcc70ae90c726832d141f5a19b2
Halim Lais <halimlais@hotmail.com>
2018-04-21 10:25:51 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day094/TrailingZerosOfN.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars5Kata
+{
+    class TrailingZerosOfN
+    {
+        public static int TrailingZeros(int n)
+        {
+
+            int timesZero = 0;
+
+            while (n >= 5)
+            {
+                timesZero += n / 5;
+                n = n / 5;
+            }
+            return timesZero;
+        }
+    }
+}
