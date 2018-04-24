2e362f3696a6975e7b627e12500945d9346d3abd
Halim Lais <halimlais@hotmail.com>
2018-04-24 21:48:37 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day096/CamelCaseMethod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class CamelCaseMethod
+    {
+        public static string CamelCase( string str)
+        {
+
+            string empty = "";
+            string[] words = str.ToLower().Split(' ');
+
+            foreach (var item in words)
+            {
+                if (item.Length > 0)
+                {
+                    empty += item.Substring(0, 1).ToUpper();
+                    empty += item.Substring(1).ToLower();
+                }
+            }
+
+            return empty;
+
+        }
+    }
+}
