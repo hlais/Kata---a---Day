8d165b1cf096499447f626380ba8d967131f3e97
Halim Lais <halimlais@hotmail.com>
2018-04-28 18:10:56 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day098/PhoneDirectory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace CodeWars5Kata
+{
+    class PhoneDirectory
+    {
+        public static string Phone(string strng, string num)
+        {
+           List <string> collectionsOfAddress = new List<string> (strng.Split('\n'));
+            int count = 0;
+            string foundNumber = "";
+            string name = "";
+            
+            for (int i = 0; i < collectionsOfAddress.Count; i++)
+            {
+                if (collectionsOfAddress[i].Contains(num))
+                {
+                    count++;
+                    foundNumber = collectionsOfAddress[i];
+                }
+            }
+
+            if (count > 1)
+            {
+                return $"Error => Too many people: {num}";
+            }
+            if (count == 0)
+            {
+                return $"Error => Not found: {num}";
+            }
+
+            int start = foundNumber.IndexOf('<')+1;
+            int end = foundNumber.IndexOf('>');
+            int length = end - start;
+
+            name = foundNumber.Substring(start, length);
+           
+            Regex charactersToRemove = new Regex("[*:;?/$<>+!,]");
+            string address = charactersToRemove.Replace(foundNumber, "").Replace(name, "").Replace(num, "").Replace("_"," ").Replace("  ", " ").Trim();
+
+            string organised = $"Phone => {num}, Name => {name}, Address => {address}";
+            return organised;
+
+        }
+    }
+}
