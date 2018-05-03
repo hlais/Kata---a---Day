ab19d38f063e9c2df27aa246eba5ce06f75b84c3
Halim Lais <halimlais@hotmail.com>
2018-05-03 22:17:13 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day099/SumOfNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class SumOfNumbers
+    {
+        public static int GetSum(int a, int b)
+        {
+            int[] numbers = { a, b };
+            int sum = 0;
+           int start = numbers.Min();
+            int fin = numbers.Max();
+
+            for (; start <= fin; start++)
+            {
+                sum += start;
+            }
+
+            return sum;
+
+        }
+    }
+}
