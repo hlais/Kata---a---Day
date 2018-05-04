71bb58689dce598ec9427b6a944118681180a0d9
Halim Lais <halimlais@hotmail.com>
2018-05-04 20:59:50 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day100/Sum Of Angles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class Sum_Of_Angles
+    {
+        public static int Angle(int n)
+        {
+            int angle = (n - 2) * 180;
+
+            return angle;
+        }
+    }
+}
