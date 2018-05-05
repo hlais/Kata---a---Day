7f89a641bf54bd6b24dfb247aa5838aa030ecdab
Halim Lais <halimlais@hotmail.com>
2018-05-05 06:51:10 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day101/Two Oldest Ages.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CodeWards7Kata
+{
+    class Two_Oldest_Ages
+    {
+        public static int[] TwoOldestAges(int[] ages)
+        {
+
+            int eldest = -1;
+            int secondEldest = -1;
+
+            foreach (var age in ages)
+            {
+
+                if (age > eldest)
+                {
+                    
+                    secondEldest = eldest;
+                    eldest = age;
+                }
+               else if (age > secondEldest)
+                {
+                    secondEldest = age;
+                }
+
+
+            }
+            int[] twoEldest = { secondEldest, eldest };
+            return twoEldest;
+
+
+        }
+    }
+}
