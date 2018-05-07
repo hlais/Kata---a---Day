c88958d17a9187585c43ed66d63d31739cdf1b75
Halim Lais <halimlais@hotmail.com>
2018-05-07 22:48:33 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day103/Categorize New Member.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class Categorize_New_Member
+    {
+        public static IEnumerable<string> OpenOrSenior(int[][] data)
+        {
+            List<string> someChars = new List<string>();
+            
+            foreach (var item in data)
+            {
+                if (item[0] >= 55 && item[1] > 7)
+                {
+                    someChars.Add("Senior");
+                }
+                else
+                    someChars.Add("Open");
+            }
+
+            return someChars;
+        }
+    }
+}
