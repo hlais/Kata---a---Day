12790361148f487d02a5989d9eb9be18c735c15f
Halim Lais <halimlais@hotmail.com>
2018-05-10 21:33:04 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day105/Strings From a Hash.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class Strings_From_a_Hash
+    {
+        public static string StringifyDict<TKey, TValue>(Dictionary<TKey, TValue> hash)
+        {
+            string empty = "";
+
+            if (hash.Count == 0 )
+            {
+                return empty;
+            }
+
+                foreach (var item in hash)
+            {
+                empty += $"{item.Key} = {item.Value},";
+                empty.TrimEnd();
+            }
+            return empty.Substring(0, empty.Length - 1);
+            
+        }
+    }
+}
