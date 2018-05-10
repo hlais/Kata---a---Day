576006e29e762bf9e1d690c90c2caeaf6eff8c84
Halim Lais <halimlais@hotmail.com>
2018-05-10 22:18:39 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day104/From the Largest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CodeWards7Kata
+{
+    class From_the_Largest
+    {
+        public static int MaxNumber(int n)
+        {
+            List<int> numbers = n.ToString().Select(x => Convert.ToInt32(x)-48).ToList();
+
+            numbers.Sort();
+             numbers.Reverse();
+
+           return Convert.ToInt32(string.Join(null, numbers));
+
+        }
+    }
+}
