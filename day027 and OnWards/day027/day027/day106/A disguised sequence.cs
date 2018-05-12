380ebaece23903b4c3dabec35fa81ca56c061c77
Halim Lais <halimlais@hotmail.com>
2018-05-12 15:11:48 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day106/A disguised sequence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+
+
+namespace CodeWars6Kata
+{
+    class A_disguised_sequence
+    {
+        public static BigInteger fcn(int n)
+        {
+            BigInteger result;
+
+            result = (BigInteger)Math.Pow(2, n);
+            return result;
+
+        }
+    }
+}
