2cd7d2617d9b140cd511500b829e6c9679332110
Halim Lais <halimlais@hotmail.com>
2018-05-14 08:35:58 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day108/Odd Or Even.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class Odd_Or_Even
+    {
+        public static string OddOrEven(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            if (sum % 2 == 0)
+            {
+                return "even";
+            }
+            else
+                return "odd";
+        }
+    }
+}
