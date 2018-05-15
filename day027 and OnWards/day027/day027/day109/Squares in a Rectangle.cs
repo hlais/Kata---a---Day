654889cc0950290b12008dddbad7717c4aad148b
Halim Lais <halimlais@hotmail.com>
2018-05-15 23:41:22 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day109/Squares in a Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Numerics;
+
+namespace CodeWars5Kata
+{
+    class Squares_in_a_Rectangle
+    {
+        public static BigInteger perimeter(BigInteger n)
+        {
+            n = n + 1;
+
+            int a = 0;
+            int b = 1;
+            int sum = 1;
+            // In N steps compute Fibonacci sequence iteratively.
+            for (int i = 0; i < n-1 ; ++i)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+                sum += b;
+            }
+            return sum * 4;
+        }
+
+    }
+}
