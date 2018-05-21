6da0a0c40f66e06b47dcfd8c2eb0b803689a6a82
Halim Lais <halimlais@hotmail.com>
2018-05-21 11:05:06 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day112/CheckingGroups.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars6Kata
+{
+    class CheckingGroups
+    {
+        public static bool validBraces(String braces)
+        {
+            string prev = "";
+            while (str.Length != prev.Length)
+            {
+                prev = str;
+                str = str
+                    .Replace("()", String.Empty)
+                    .Replace("[]", String.Empty)
+                    .Replace("{}", String.Empty);
+            }
+            return (str.Length == 0);
+
+        }
+
+    }
+}
+    
+
