17d8c0ff818372ed93e5679f84fd2c76e492d3d7
Halim Lais <halimlais@hotmail.com>
2018-05-22 09:48:03 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day113/FindNextSquare.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class FindNextSquare
+    {
+        public static long NextSquare(long num)
+        {
+            
+            long nextSquare = 0;
+            if (Math.Sqrt(num) % 1 != 0)
+            {
+                return -1;
+            }
+            else
+            {
+                long currentSqr = (long)Math.Sqrt(num);
+                nextSquare = currentSqr + 1;
+
+            }
+            return nextSquare * nextSquare;
+        }
+    }
+}
