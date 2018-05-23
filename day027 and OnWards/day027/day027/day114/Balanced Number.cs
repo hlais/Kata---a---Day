6c3b9653b7380a785b7949b4664bacbc1f83fe15
Halim Lais <halimlais@hotmail.com>
2018-05-23 11:46:15 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day114/Balanced Number.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Numerics;
+
+namespace CodeWards7Kata
+{
+    class Balanced_Number
+    {
+        public static string BalancedNumber(int number)
+        {
+            string numberStr = number.ToString();
+            int pad = 0;
+            int leftSide = 0;
+            int rightSide = 0;
+            
+            if ((decimal)numberStr.Length / 2 % 1 == 0)
+            {
+                pad = (numberStr.Length / 2) - 1; 
+            }
+            else
+                pad = (numberStr.Length / 2) ;
+
+            if (numberStr.Length <= 2)
+            {
+                return "Balanced";
+            }
+            else
+            {
+                string leftSideStr = numberStr.Substring(0, pad);
+                string rightSideStr = numberStr.Substring(numberStr.Length - leftSideStr.Length, pad);
+                leftSide= leftSideStr.Select(x => Convert.ToInt32(x)-48).Sum();
+                rightSide = rightSideStr.Select(x => Convert.ToInt32(x)-48).Sum();
+            }
+            return leftSide == rightSide ? "Balanced" : "Not Balanced";
+            
+
+        }
+    }
+}
