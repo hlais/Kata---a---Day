4de24337a6e5ff9f30b29e1e87da9c435eb6c4cc
Halim Lais <halimlais@hotmail.com>
2018-05-24 14:40:46 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day115/Strong Number.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWards7Kata
+{
+    class Strong_Number
+    {
+        public static string StrongNumber(int number)
+        {
+            int sum = 0;
+            char[] chaNumber = number.ToString().ToCharArray();
+            List<int> collectionNum = new List<int>();
+            foreach (var item in chaNumber)
+            {
+               collectionNum.Add(Convert.ToInt32(item - 48));
+            }
+
+            foreach (var item in collectionNum)
+            {
+                int iteration = item;
+                int curFactorial = 1;
+                while (iteration > 0)
+                {
+                    curFactorial *= iteration;
+                    iteration--;
+                }
+                sum += curFactorial;
+            }
+            return sum == number ? "STRONG!!!!" : "Not Strong !!";
+        }
+    }
+}
