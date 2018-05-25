19f5008b9ddcfc6f562432949d15216aa21aa27e
Halim Lais <halimlais@hotmail.com>
2018-05-25 16:47:10 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day116/Disarium Number.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace CodeWards7Kata
+{
+    class Disarium_Number
+    {
+        public static string DisariumNumber(int number)
+        {
+            int sum = 0;
+            int[] numberAr = Array.ConvertAll(number.ToString().ToCharArray(), x => Convert.ToInt32(x) - 48);
+            
+            for (int i = 0; i < numberAr.Length; i++)
+            {
+               sum+= (int) Math.Pow(numberAr[i],(i+1));
+            }
+            return sum == number ? "Disarium !!" : "Not !!";
+        }
+    }
+}
