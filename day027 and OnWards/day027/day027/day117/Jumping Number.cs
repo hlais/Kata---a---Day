8cb908e5c037f523bdc18014bd1a4a7243ad9c1d
Halim Lais <halimlais@hotmail.com>
2018-05-27 00:02:51 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day117/Jumping Number.cs
@@ -0,0 +1,29 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class Jumping_Number
+    {
+        public static string JumpingNumber(int number)
+        {
+            int[] arrNumber = Array.ConvertAll(number.ToString().ToCharArray(), x => Convert.ToInt32(x) - 48);
+
+            if (arrNumber.Length < 1)
+            {
+                return "Jumping!!";
+            }
+
+            for (int i = 1; i < arrNumber.Length; i++)
+            {
+                int previousNumber = arrNumber[i - 1];
+                if (Math.Abs(previousNumber - arrNumber[i]) != 1)
+                {
+                    return "Not!!";
+                }
+            }
+            return "Jumping!!";
+        }
+    }
+}
