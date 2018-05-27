92db273bfa0abbe1069febb605b95e874014befa
Halim Lais <halimlais@hotmail.com>
2018-05-27 11:02:41 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day118/Special Number.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class Special_Number
+    {
+        public static string SpecialNumber(int number)
+        {
+            int[] numbersArry = Array.ConvertAll(number.ToString().ToCharArray(), x => Convert.ToInt32(x) - 48);
+
+            for (int i = 0; i < numbersArry.Length; i++)
+            {
+                if (numbersArry[i] > 5)
+                {
+                    return "NOT!!";
+                }
+            }
+            return "Special!!";
+        }
+    }
+}
