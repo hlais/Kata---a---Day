4854e8c4092442c2de5d50cebf52350a3b9c6353
Halim Lais <halimlais@hotmail.com>
2018-05-28 19:17:10 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day119/TransformToPrime.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class TransformToPrime
+    {
+        public static int MinimumNumber(int[] numbers)
+        {
+            int incrementCounter = 0;
+            int sum =  numbers.Sum();
+
+            while (!isPrimeNumber(sum))
+            {
+                sum += 1;
+                incrementCounter++;
+            }
+            return incrementCounter;
+        }
+
+
+        public static bool isPrimeNumber(int number)
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
