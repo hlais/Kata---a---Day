d79e3e438591e2c24a3b1bd45a78eb4c584a31c0
Halim Lais <halimlais@hotmail.com>
2018-05-30 12:40:19 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day121/Fizz Buzz/Fizz Buzz.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class Fizz_Buzz
+    {
+        public static string[] GetFizzBuzzArray(int n)
+        {
+            List<string> fizzBuzz = new List<string>();
+
+            if (n < 1)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+                for (int i = 1; i <= n; i++)
+                {
+                    string results = "";
+
+                    if (i % 15 == 0)
+                    {
+                        results = "FizzBuzz";
+                    }
+                    else if (i % 3 == 0)
+                    {
+                        results = "Fizz";
+                    }
+                    else if (i % 5 == 0)
+                    {
+                        results = results + "Buzz";
+                    }
+                    else
+                    {
+                        results = i.ToString();
+                    }
+
+                    fizzBuzz.Add(results);
+                }
+            return fizzBuzz.ToArray();
+        }
+    }
+}
+
+
---
+++ day027 and OnWards/day027/day027/day121/Maximum Multiple/Maximum Multiple.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class Maximum_Multiple
+    {
+        public static int MaxMultiply(int divisor, int bound)
+        {
+            int maximumMultiple = (bound / divisor) * divisor;
+            return maximumMultiple;
+        }
+    }
+}
