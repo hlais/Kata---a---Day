bf2b7bb5a1131e0fffb90121d7a118fc396a61e3
Halim Lais <halimlais@hotmail.com>
2018-05-31 18:35:55 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day122/MostFequentInArray.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars6Kata
+{
+    class MostFequentInArray
+    {
+        public static int HighestRank(int[] arr)
+        {
+            int freqCount = 0;
+            int highestFreqNum = 0;
+            List<int> listOfmx = new List<int>(arr);
+            listOfmx.Sort();
+
+            foreach (var item in listOfmx)
+            {
+               int  curfreqCount = (arr.Count(x => x == item));
+
+                if (curfreqCount >= freqCount)
+                {
+                    freqCount = curfreqCount;
+                    highestFreqNum= item;
+                } 
+            }
+            return highestFreqNum;
+        }
+    }
+}
