cc00c16b760e74c4d7d574b0deb787ba65e5f5e9
Halim Lais <halimlais@hotmail.com>
2018-06-01 12:02:01 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day123/SomeOfOddNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class SomeOfOddNumbers
+    {
+        public static long rowSumOddNumbers(long n)
+        {
+            long start = ((n * n) - n) + 1;
+            int counter = 1;
+            long sumOfOdd = start;
+
+            while (counter < n)
+            {
+                long incre =  start += 2;
+                sumOfOdd += incre;
+                counter++;
+            }
+
+            return sumOfOdd;
+        }
+    }
+}
