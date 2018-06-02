270ab65034fd9fcb7fc464939d73543e9afc081e
Halim Lais <halimlais@hotmail.com>
2018-06-02 16:07:00 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day124/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class FizzBuzz
+    {
+        public static int[] Solution(int number)
+        {
+            int counter = 1;
+            int arr0 = 0;
+            int arr1 = 0;
+            int arr2 = 0;
+
+            while (counter < number)
+            {
+                if (counter % 15 == 0)
+                {
+                    arr2++;
+                }
+                else if (counter % 5 == 0)
+                {
+                    arr1++;
+                }
+                else if (counter %3 ==0)
+                {
+                    arr0++;
+                }
+                counter++;
+            }
+
+            int[] fizzBuzzCounter = { arr0, arr1, arr2 };
+            return fizzBuzzCounter;
+        }
+    }
+}
---
+++ day027 and OnWards/day027/day027/day124/StringCalculator/StringCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeWards7Kata
+{
+    class StringCalculator
+    {
+        public static int AddStrings(string numbers)
+        {
+
+           return numbers.Split(',').Select(x => Convert.ToInt32(x)).Sum();
+        }
+    }
+}
