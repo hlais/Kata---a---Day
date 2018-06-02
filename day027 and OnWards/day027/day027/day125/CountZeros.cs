c84456b8f0c8013703a8c6800016cdc4e93bd8d1
Halim Lais <halimlais@hotmail.com>
2018-06-02 16:03:47 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day125/CountZeros.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CodeWars6Kata
+{
+    class CountZeros
+    {
+        public static ulong CountZeroes(long n)
+        {
+
+           ulong limit= (9 *((ulong)n-1)-1) *(ulong)Math.Pow(10, n-1) +1;
+            return limit/9;
+        }
+    }
+}
