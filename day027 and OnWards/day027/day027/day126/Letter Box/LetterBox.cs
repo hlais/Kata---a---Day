221c87ffe15ac6768c10f02c36d8b7282779b1b8
Halim Lais <halimlais@hotmail.com>
2018-06-04 14:41:17 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day126/Letter Box/LetterBox.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class LetterBox
+    {
+        public static IEnumerable<int> PaintLetterBoxes(int start, int end)
+        {
+            int[] numberCounter = new int[10];
+            for (int i = start; i <= end; i++)
+            {
+
+                string number = i.ToString();
+
+                    foreach (var item in number)
+                    {
+                        numberCounter[(int)char.GetNumericValue(item)]++;
+                    }
+
+            }
+            return numberCounter;
+        }
+    }
+}
