ba119cb8e2555885acaaceabeb9790294e779dee
Halim Lais <halimlais@hotmail.com>
2018-06-05 22:29:20 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day127/TwoSum/Two_Sum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class Two_Sum
+    {
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] + numbers[j] == target && i !=j)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
