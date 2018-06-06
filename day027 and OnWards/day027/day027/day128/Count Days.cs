20955f84bac60cecd69bf48097aca67e839a823e
Halim Lais <halimlais@hotmail.com>
2018-06-06 17:30:42 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day128/Count Days.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+
+
+
+namespace CodeWars6Kata
+{
+    class Count_Days
+    {
+        static public string countDays(DateTime d)
+        {
+
+            DateTime todaysDate = DateTime.Now;
+
+            double days =  Math.Truncate((d - todaysDate).TotalDays);
+            string strgDays = days.ToString();
+
+            if (strgDays.Contains('-'))
+            {
+                return "The day is in the past!";
+            }
+            else if (strgDays == "0")
+            {
+                return "Today is the day!";
+            }
+            else 
+
+            return $"{days.ToString()} days";
+
+        }
+    }
+}
