ad0faa9cd97aaf7f6a95cc557a0510c03acc0549
Halim Lais <halimlais@hotmail.com>
2018-06-08 14:05:56 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day129/Minutes to Midnight.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class Minutes_to_Midnight
+    {
+        static public string countMinutes(DateTime d)
+        {
+            TimeSpan midnight = new TimeSpan(24, 00, 00);
+
+            TimeSpan midDifTime = midnight.Subtract(d.TimeOfDay);
+
+            double diff = Math.Round(midDifTime.TotalMinutes);
+
+            if (diff < 10)
+            {
+                return $"{diff} minute";
+            }
+            else
+                return $"{diff} minutes";
+
+        }
+    }
+}
