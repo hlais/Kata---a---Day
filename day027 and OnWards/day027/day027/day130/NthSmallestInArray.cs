419faaf4f7861725265a29bb4296004df00a6356
Halim Lais <halimlais@hotmail.com>
2018-06-10 10:06:23 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day130/NthSmallestInArray.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CodeWards7Kata
+{
+    class NthSmallestInArray
+    {
+        public static int NthSmallest(int[] arr, int pos)
+        {
+             var sorted = arr.OrderBy(x => x).ToArray();
+              return  sorted.ElementAt(pos);
+            
+        }
+    }
+}
