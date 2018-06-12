db8be342153409d194e8e7be9040866ac4efe7b1
Halim Lais <halimlais@hotmail.com>
2018-06-12 14:14:39 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day131/Rectangle Into Squares.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class Rectangle_Into_Squares
+    {
+        public static List<int> sqInRect(int lng, int wdth)
+        {
+            List<int> squareArea = new List<int>();
+
+            if (lng == wdth)
+            {
+                return null;
+            }
+            while (true)
+            {
+               
+                int sqr= lng > wdth ? wdth : lng;
+                squareArea.Add(sqr);
+
+                if (lng > wdth)
+                {
+                    lng -= sqr;
+                   
+                }
+                else
+                {
+                    wdth -= sqr;
+
+                }
+                //the final block 
+                if (lng == wdth)
+                {
+                    squareArea.Add(lng);
+                    break;
+                }
+            }
+            return squareArea;
+        }
+    }
+}
