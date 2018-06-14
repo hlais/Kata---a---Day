8b241a9fbf32af671fa53a16bfcc538c808d15b1
Halim Lais <halimlais@hotmail.com>
2018-06-14 22:59:56 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day132/GrowthOfPopulation/GrowthOfAPopulation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class GrowthOfAPopulation
+    {
+        public static int NbYear(int p0, double percent, int aug, int p)
+        {
+            int expectedPopulation = p;
+
+            int cityPopulation = p0;
+            int newInhabitantsPerYear = aug;
+            double increasePerYear = percent / 100;
+            int years = 0;
+
+            while (cityPopulation < expectedPopulation)
+            {
+                cityPopulation += (int)(cityPopulation * increasePerYear) + newInhabitantsPerYear;
+
+                years++;
+            }
+
+            return years;
+        }
+    }
+}
