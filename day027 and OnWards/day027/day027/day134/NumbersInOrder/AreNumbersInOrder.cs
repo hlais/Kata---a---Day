cfc71993fa5f786a37abb46c5f944559a6f53abc
Halim Lais <halimlais@hotmail.com>
2018-06-16 20:09:53 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day134/NumbersInOrder/AreNumbersInOrder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class AreNumbersInOrder
+    {
+        public static bool IsAscOrder(int[] arr)
+        {
+            int previousNumber = 0;
+            foreach (var item in arr)
+            {
+                if (item >= previousNumber)
+                {
+                    previousNumber = item;
+                }
+                else
+                    return false;
+            }
+            return true;
+        }
+    }
+}
