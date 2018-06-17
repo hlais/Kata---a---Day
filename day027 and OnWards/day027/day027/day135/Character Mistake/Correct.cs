77aa3fecbf932d544b30c31bcdbcf0dfc69a72d0
Halim Lais <halimlais@hotmail.com>
2018-06-17 22:33:53 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day135/Character Mistake/Correct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class Correct
+    {
+        
+        public static string CorrectMistakes(string text)
+        {
+            return text.Replace('0', 'O').Replace('1', 'I').Replace('5','S');
+           
+        }
+    }
+}
