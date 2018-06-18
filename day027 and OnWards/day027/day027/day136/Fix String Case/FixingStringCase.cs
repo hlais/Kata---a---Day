d8ad15c351ae016798889deb21ff8c162b7d7e28
Halim Lais <halimlais@hotmail.com>
2018-06-18 23:52:59 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day136/Fix String Case/FixingStringCase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class FixingStringCase
+    {   
+        public static string Solve(string s)
+        {
+            int lowerCharCount = 0;
+            int upperCharCount = 0;
+            foreach (var item in s)
+            {
+                if (char.IsLower(item))
+                {
+                    lowerCharCount++;
+                }
+                else
+                    upperCharCount++;
+            }
+
+            if (lowerCharCount > upperCharCount || lowerCharCount == upperCharCount)
+            {
+                return s.ToLower();
+            }
+            else
+                return s.ToUpper();
+            
+        }
+    }   
+}
