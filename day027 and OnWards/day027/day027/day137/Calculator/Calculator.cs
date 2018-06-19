dda3211d755b04354e53ea52e422053a38be617a
Halim Lais <halimlais@hotmail.com>
2018-06-19 20:50:21 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day137/Calculator/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace CodeWars3Kata
+{
+    class Calculator
+    {
+        public static double Evaluate(string expression)
+        {
+            string answer = new DataTable().Compute(expression, null).ToString();
+           return Convert.ToDouble(answer);
+        }
+    }
+}
