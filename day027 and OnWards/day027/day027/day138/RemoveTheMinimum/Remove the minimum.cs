5bfda2cc31daaa4ef889699c371c2566747a9e1d
Halim Lais <halimlais@hotmail.com>
2018-06-22 11:35:20 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day138/RemoveTheMinimum/Remove the minimum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CodeWards7Kata
+{
+    class Remove_the_minimum
+    {
+        public static List<int> RemoveSmallest(List<int> numbers)
+        {
+
+            int smallestNumber = int.MaxValue;
+            int indexOfSmallest = 0;
+
+            if (numbers.Count == 0)
+            {
+                return new List<int>();
+            }
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] < smallestNumber)
+                {
+                    smallestNumber = numbers[i];
+                    indexOfSmallest = i;
+                }
+            }
+            numbers.RemoveAt(indexOfSmallest);
+
+            return numbers;
+          
+        }
+    }
+}
