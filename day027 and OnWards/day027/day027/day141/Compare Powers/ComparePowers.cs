76c57696a043958d6cbe8db14b38c5396a5afff1
Halim Lais <halimlais@hotmail.com>
2018-06-26 22:42:59 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day141/Compare Powers/ComparePowers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+
+namespace CodeWars6Kata
+{
+    class ComparePowers
+    {
+        public static int comparePowers(int[] a, int[] b)
+        {
+         
+            BigInteger groupAPowers = (BigInteger)Math.Pow(a[0], a[1]);
+            BigInteger groupBPowers = (BigInteger)Math.Pow(b[0], b[1]);
+
+            if (groupAPowers == groupBPowers)
+            {
+                return 0;
+            }
+            return groupAPowers > groupBPowers ? -1 : 1;
+        }
+    }
+}
