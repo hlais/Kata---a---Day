e63d2f5347d2afb2396e878ae79d7c78659db903
Halim Lais <halimlais@hotmail.com>
2018-07-02 23:41:31 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day144/DetectPangram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class DetectPangram
+    {
+        public static bool IsPangram(string str)
+        {
+            
+            char[] charStr = str.ToLower().ToCharArray();
+            List<char> alphapet = new List<char>();
+
+            foreach (var item in charStr)
+            {
+                if (alphapet.Contains(item) || char.IsLetter(item) == false)
+                {
+                    continue;
+                }
+                alphapet.Add(item);
+
+
+            }
+
+            return alphapet.Count == 26 ? true : false;  
+            
+        }
+    }
+}
