3d4f11db60d17606c9cd3d212b119bdd9b76e5cc
Halim Lais <halimlais@hotmail.com>
2018-07-03 23:00:11 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day145/MexicanWave.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class MexicanWave
+    {
+        public static List<string> wave(string str)
+        {
+           
+            List<string> listOfWaves = new List<string>();
+            
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                char []strToChars = str.ToLower().ToCharArray();
+
+                if (char.IsLetter(strToChars[i]))
+                {
+
+                   strToChars[i] = char.ToUpper(strToChars[i]);
+                    listOfWaves.Add(new string(strToChars));
+
+                }
+                else
+                    continue;
+            }
+            return listOfWaves;
+
+        }
+    }
+}
