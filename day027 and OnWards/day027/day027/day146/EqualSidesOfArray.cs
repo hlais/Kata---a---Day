9aed137a9f7a1795b0e81d98b714eeeba64e74d9
Halim Lais <halimlais@hotmail.com>
2018-07-04 13:18:46 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day146/EqualSidesOfArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class EqualSidesOfArray
+    {
+        public static int FindEvenIndex(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int leftSide = arr.Take(i).Sum();
+                int rightSide = arr.Skip(i+1).Sum();
+
+                if (leftSide == rightSide)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+
+        } 
+    }
+}
