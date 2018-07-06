bed6103f1fb84cea8e2c63fae186bb64f791f240
Halim Lais <halimlais@hotmail.com>
2018-07-06 11:56:35 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day147/Sum Consecutives.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class Sum_Consecutives
+    {
+        public static List<int> SumConsecutives(List<int> s)
+        {
+            List<int> results = new List<int>();
+
+            int consecutiveSum = 0;
+
+            for (int i = 0; i < s.Count; i++)
+            {
+
+                consecutiveSum += s[i];
+
+                if (s[i]== s[s.Count-1]|| s[i] != s[i + 1])
+                {
+                    results.Add(consecutiveSum);
+                    consecutiveSum = 0;
+                }
+
+            }
+            return results;
+        }
+    }
+}
