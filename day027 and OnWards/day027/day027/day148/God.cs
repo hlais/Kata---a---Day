0a9ca022d2029389d890af694e17cf2aee39bacd
Halim Lais <halimlais@hotmail.com>
2018-07-07 11:10:49 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day148/God.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    public class God
+    {
+        public static Human[] Create()
+        {
+            Man man = new Man();
+            Woman woman = new Woman();
+
+            Human[] humans = new Human[] { man, woman };
+            return humans;
+        }
+    }
+    public class Human
+    {
+
+    }
+    public class Man : Human
+    {
+    }
+    public class Woman : Human
+    {
+    }
+}
---
+++ day027 and OnWards/day027/day027/day148/Ship.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class Ship
+    {
+        public int Draft;
+        public int Crew;
+       
+
+        public Ship(int draft, int crew)
+        {
+            Draft = draft;
+            Console.WriteLine(draft);
+            Crew = crew;
+            Console.WriteLine(crew);
+        }
+
+        public bool IsWorthIt()
+        {
+            return this.Draft - this.Crew * 1.5 >= 20;
+            
+        }
+
+    }
+}
