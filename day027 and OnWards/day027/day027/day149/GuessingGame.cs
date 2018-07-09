4a8be9faafb3361e1dbe169828a4c883d675a639
Halim Lais <halimlais@hotmail.com>
2018-07-09 22:07:31 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day149/GuessingGame.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class GuessingGame
+    {
+        private int number;
+        private int lives;
+
+        public GuessingGame(int number, int lives)
+        {
+            this.number = number;
+            this.lives = lives;
+        }
+
+        public bool Guess(int n)
+        {
+            if (lives <= 0)
+            {
+                throw new Exception();
+            }
+           
+                bool condition = (n == number);
+                if (!condition)
+                {
+                    this.lives--;
+                }
+                return condition;
+            }
+         
+            
+        }
+    }
+
+
---
+++ day027 and OnWards/day027/day027/day150/Classy Extentions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    public class Cat : Animal
+    {
+        string name;
+        public Cat(string name) : base(name)
+        {
+            this.name = name;
+        }
+
+        public override string Speak()
+        {
+            return name + " meows.";
+        }
+    }
+}
