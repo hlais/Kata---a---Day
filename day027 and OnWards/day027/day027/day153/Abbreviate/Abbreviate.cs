737c30cddccc158c08caa50f676978146790d7ed
Halim Lais <halimlais@hotmail.com>
2018-07-16 21:43:36 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day153/Abbreviate/Abbreviate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class Abbreviate
+    {
+        public static string AbbrevName(string name)
+        {
+            string[] subName = name.Split(' ');
+           return  subName[0][0].ToString().ToUpper() + "." + subName[1][0].ToString().ToUpper();
+    
+        }
+
+    }
+}
---
+++ day027 and OnWards/day027/day027/day153/Boolean To String/BoolToString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class BoolToString
+    {
+        public static string boolean_to_string(bool b)
+        {
+            return b ? "True" : "False";
+        }
+    }
+}
