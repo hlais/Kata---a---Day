003181685651014613dc56c5819f65c2ef9b0be9
Halim Lais <halimlais@hotmail.com>
2018-07-30 21:56:54 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day157/ReplaceWords.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class ReplaceWords
+    {
+        public static string Covfefe(string tweet)
+        {
+            if (tweet.Contains("coverage"))
+            {
+                return tweet.Replace("coverage", "covfefe");
+            }
+            else
+                return tweet + " covfefe";
+                
+            
+        }
+    }
+}
