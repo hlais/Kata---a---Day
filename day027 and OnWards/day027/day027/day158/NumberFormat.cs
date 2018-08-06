3813003b36b345fe0e42c62e3b7ccf4688219064
Halim Lais <halimlais@hotmail.com>
2018-08-06 17:24:53 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day158/NumberFormat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6Kata
+{
+    class NumberFormat
+    {
+        public static string numberFormat(int number)
+        {
+            string stringNumber = number.ToString("N0");
+            return stringNumber;
+        }
+    }
+}
