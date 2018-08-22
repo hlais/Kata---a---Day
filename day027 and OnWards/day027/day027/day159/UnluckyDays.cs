02a0af358d2c2d78c80c1257fc1ee42ffcbe51ab
Halim Lais <halimlais@hotmail.com>
2018-08-22 10:51:10 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day159/UnluckyDays.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class UnluckyDays
+    {
+        public static int UnluckyDaysInYear(int year)
+        {
+
+            int friday13thCounter = 0;
+
+            for (int i = 1; i <= 12; i++)
+            {
+                DateTime monthIteratar = new DateTime(year, i, 13);
+
+                if (monthIteratar.DayOfWeek == DayOfWeek.Friday)
+                {
+                    friday13thCounter++;
+                }
+            }
+             return friday13thCounter;
+        }
+    }
+}
+
