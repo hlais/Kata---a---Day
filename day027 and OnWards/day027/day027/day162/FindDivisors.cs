78d325ab461f15d3a681ab659fc17d5ab450e5a2
Halim Lais <halimlais@hotmail.com>
2018-08-25 09:31:02 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day162/FindDivisors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class FindDivisors
+    {
+        public static int Divisors(int n)
+        {
+            int divisorCount = 0;
+            if (n == 0)
+            {
+                return 0;
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    divisorCount++;
+                }
+
+            }
+            return divisorCount;
+        }
+    }
+}
