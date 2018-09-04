ce4fe4dd1092857f4e8a2bb2b85299bb2ec0f8c8
Halim Lais <halimlais@hotmail.com>
2018-09-04 21:06:05 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day163/ElapsedSeconds.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWards7Kata
+{
+    class ElapsedSeconds
+    {
+        public static int ElapsedSecondsCal (DateTime startDate, DateTime endDate)
+        {
+
+           TimeSpan elapsed = endDate - startDate;
+            return (int)elapsed.TotalSeconds;
+        }
+    }
+}
