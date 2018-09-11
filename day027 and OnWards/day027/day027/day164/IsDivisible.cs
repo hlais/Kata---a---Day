9f33362a4ef0f76573cbe9163238756e43682d8d
Halim Lais <halimlais@hotmail.com>
2018-09-11 11:10:25 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day164/IsDivisible.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class IsDivisible
+    {
+        public static bool IsDivisibleMethod(int wallLength, int pixelSize)
+        {
+            //    float test = (float)wallLength / pixelSize;
+
+            //    Console.WriteLine(test);
+            return (float)wallLength / pixelSize % 1 ==0 ? true:false;
+        }
+    }
+}
