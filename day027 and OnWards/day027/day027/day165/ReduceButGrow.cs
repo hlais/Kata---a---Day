1e4b80f0800e7b55aaa81204124e7dc2afc24eab
Halim Lais <halimlais@hotmail.com>
2018-09-16 21:13:33 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day165/ReduceButGrow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class ReduceButGrow
+    {
+        public static int Grow(int[] x)
+        {
+            int firstNum = x[0];
+            for (int i = 1; i < x.Length; i++)
+            {
+              firstNum=  firstNum* x[i];
+            }
+            return firstNum;
+        }
+    }
+}
