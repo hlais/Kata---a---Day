9ff64046e5e98f19dbe06cd071a8f45be9468b81
Halim Lais <halimlais@hotmail.com>
2018-09-17 10:04:53 +0100

    Update SwitchCase.cs

--- day027 and OnWards/day027/day027/day166/SwitchCase.cs
+++ day027 and OnWards/day027/day027/day166/SwitchCase.cs
@@ -6,5 +6,29 @@ namespace CodeWars
 {
     class SwitchCase
     {
+        double result = 0;
+            switch (operation)
+            {
+                case '+':
+
+                    result = value1 + value2;
+                    break;
+                case '-':
+                    result = value1 - value2;
+                    break;
+                case '/':
+                    result = value1 / value2;
+                    break;
+                case '*':
+                    result = value1 * value2;
+                    break;
+                case '%':
+                    result = value1 % value2;
+                    break;
+                case '^':
+                    result = Math.Pow(value1, value2);
+                    break;
+            }
+            return result;
     }
 }
