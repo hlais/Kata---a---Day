45f10b48a0790a60a0ad06bff8650f3be0ab0fd4
Halim Lais <halimlais@hotmail.com>
2018-09-18 10:39:31 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day167/SeatsInTheater.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class SeatsInTheater
+    {
+        public static int SeatsInTheaterMethod(int nCols, int nRows, int col, int row)
+        {
+            return (nCols - (col - 1)) * (nRows - row);
+            
+            
+        }
+    }
+}
