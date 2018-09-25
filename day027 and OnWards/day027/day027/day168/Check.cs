23448c228743ff1783c20531d06b0ff8df3efdd8
Halim Lais <halimlais@hotmail.com>
2018-09-25 21:53:10 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day168/Check.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Kata8
+{
+    class Check
+    {
+        public static bool CheckMethod(object[] a, object x)
+        {
+          
+
+            foreach (var item in a)
+            {
+                if (item.Equals(x))
+                {
+                    return true;
+                }
+            }
+            return false;
+            
+        }
+    }
+}
