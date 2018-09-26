946b6534fab84fa450fa7e78fef6a2b3ecf5da1b
Halim Lais <halimlais@hotmail.com>
2018-09-26 11:10:33 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day169/RentalCar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata8
+{
+    class RentalCar
+    {
+        public static int RentalCarCost(int d)
+        {
+            int cost = d * 40;
+            if (d >= 7)
+            {
+                return cost - 50;
+            }
+            else if (d >= 3)
+            {
+                return cost - 20;
+            }
+            else return cost;
+  
+        }
+    }
+}
+
