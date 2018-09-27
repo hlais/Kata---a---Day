4bc1e5167ecb3929785f2fa2e0603d009c93a1b4
Halim Lais <halimlais@hotmail.com>
2018-09-27 10:29:37 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day170/SuperSize.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata8
+{
+    class SuperSize
+    {
+        public static long SuperSizeMethod(long num)
+        {
+            char[] chaNums = num.ToString().ToCharArray();
+            Array.Sort(chaNums);
+           var organisedChars = chaNums.Reverse();
+            string strOrganised  = string.Concat(organisedChars);
+            long.TryParse(strOrganised, out long result);
+            return result;
+  
+
+        }
+    }
+}
+
+
