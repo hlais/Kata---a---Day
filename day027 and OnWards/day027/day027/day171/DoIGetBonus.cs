7c2c825e327e47b1e2dd27080acf49e31df688c6
Halim Lais <halimlais@hotmail.com>
2018-09-29 09:04:24 +0100

    Add files via upload

---
+++ day027 and OnWards/day027/day027/day171/DoIGetBonus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    class DoIGetBonus
+    {
+        public static string bonus_time(int salary, bool bonus)
+        {
+
+           return bonus ? $"${(salary * 10)}" : $"${salary}";
+        }
+    }
+}
